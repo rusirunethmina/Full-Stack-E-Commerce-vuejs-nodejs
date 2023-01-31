1b11354dcfaedfedd91237203918cfcac107d264
Rusiru1998 <rusiruc21@gmail.com>
2023-01-31 23:55:26 +0530

    backend inital

--- backend/src/server.js
+++ backend/src/server.js
@@ -114,7 +114,24 @@ app.get('/api/products/:productId', (req, res) => {
     } else {
         res.status(404).json('Could not find the product!');
     }
-})
+});
+
+app.post('/api/users/:userId/cart', (req, res) => {
+  const { productId } = req.body;
+  const product = products.find(product => product.id === productId);
+  if (product) {
+    cartItems.push(product);
+    res.status(200).json(cartItems);
+  } else {
+    res.status(404).json('Could not find product!');
+  }
+});
+
+app.delete('/api/users/:userId/cart/:productId', (req, res) => {
+  const { productId } = req.params;
+  cartItems = cartItems.filter(product => product.id !== productId);
+  res.status(200).json(cartItems);
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
