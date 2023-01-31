f4b66ac868577aa50205a752dfb1a20dbf207aac
Rusiru1998 <rusiruc21@gmail.com>
2023-01-31 13:23:49 +0530

    backend inital

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => {
+    res.send('Hello!');
+});
+
+app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
